53bbcee772539374dd81d32a3fc06c619e74f666
Akbar <akbarasghari@outlook.com>
2024-07-22 19:40:47 +0330

    Some Updates

--- PooyesheSabz.Web/Components/Custom/BaseButton.razor.cs
+++ PooyesheSabz.Web/Components/Custom/BaseButton.razor.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Components.Web;
-using MudBlazor;
-using static MudBlazor.Colors;
 
 namespace PooyesheSabz.Web.Components.Custom;
 
